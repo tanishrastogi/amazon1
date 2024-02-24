1e8437bc735591b0cc29a5de8b78fb19503fa32f
charlie0054A <128181459+charlie0054A@users.noreply.github.com>
2024-02-24 14:13:43 +0530

    Update slide.js 1

--- src/components/home/slide.js
+++ src/components/home/slide.js
@@ -39,7 +39,7 @@ function Slide({ productInfo, title }) {
 
     if (datas != "") {
     // console.log(datas);
-
+        console.log(product);
         return <div className="products_div">
             <div className="products_section">
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
